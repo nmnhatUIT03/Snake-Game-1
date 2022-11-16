c4a5292ae295c758334747a8ae6d80e8291dc50a
UIT_20521799 <111016584+hoangquan2002@users.noreply.github.com>
2022-11-16 21:28:42 +0700

    Add files via upload

---
+++ Snake-Game/Snake-Game/Snake-Game/Snake-Game.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+/*
+ * Import library
+ */
+#include <vector>
+#include <windows.h>
+#include <conio.h>
+#include <cstring>
+#include <time.h>
+#include <random>
+
+ /*
+  * MACRO
+  */
+#define WIDTH 40
+#define HEIGHT 20
+#define BODY '*'
+#define APPLE 'O'
+
+  /*
+   * Enum
+   */
